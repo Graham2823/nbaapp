deab344d18f2b4acfbd39ec670c446241c4eab17
Graham <gmfreundlich@gmail.com>
2023-11-14 15:29:06 -0500

    added blocks to comparison

--- pages/PlayerDetails.js
+++ pages/PlayerDetails.js
@@ -17,15 +17,14 @@ const PlayerDetails = () => {
 	const [firstName, setFirstName] = useState('');
 	const [lastName, setLastName] = useState('');
 	const [showGamelog, setShowGamelog] = useState(false);
-	const [p1FirstName, setP1FirstName] = useState('')
-	const [p1LastName, setP1LastName] = useState('')
-	const [p2FirstName, setP2FirstName] = useState('')
-	const [p2LastName, setP2LastName] = useState('')
-	const [p1Data, setP1Data] = useState([])
-	const [p2Data, setP2Data] = useState([])
-	const [p1Stats, setP1Stats] = useState([])
-	const [p2Stats, setP2Stats] = useState([])
-
+	const [p1FirstName, setP1FirstName] = useState('');
+	const [p1LastName, setP1LastName] = useState('');
+	const [p2FirstName, setP2FirstName] = useState('');
+	const [p2LastName, setP2LastName] = useState('');
+	const [p1Data, setP1Data] = useState([]);
+	const [p2Data, setP2Data] = useState([]);
+	const [p1Stats, setP1Stats] = useState([]);
+	const [p2Stats, setP2Stats] = useState([]);
 
 	const handleSubmit = () => {
 		axios
@@ -37,28 +36,33 @@ const PlayerDetails = () => {
 			})
 			.catch((error) => {
 				console.error('Error fetching data:', error);
-                toast.error('Player Not Found!')
+				toast.error('Player Not Found!');
 			});
 	};
 
-	const comparePlayers = () =>{
-		axios.get(`https://nbaapp.vercel.app/api/comparePlayers?p1Name=${p1FirstName}_${p1LastName}&p2Name=${p2FirstName}_${p2LastName}`)
-		.then((response)=>{
-			console.log(response.data)
-			if(response.data === 'Could not find one of the players'){
-				toast.error("Could Not find one of the players. Please check spelling, and try again")
-			}else{
-				setP1Data(response.data.p1Data)
-				setP2Data(response.data.p2Data)
-				setP1Stats(response.data.p1Stats)
-				setP2Stats(response.data.p2Stats)
-			}
-		})
-		.catch((error)=>{
-			console.log(error)
-			toast.error('Players could not be found!')
-		})
-	}
+	const comparePlayers = () => {
+		axios
+			.get(
+				`https://nbaapp.vercel.app/api/comparePlayers?p1Name=${p1FirstName}_${p1LastName}&p2Name=${p2FirstName}_${p2LastName}`
+			)
+			.then((response) => {
+				console.log(response.data);
+				if (response.data === 'Could not find one of the players') {
+					toast.error(
+						'Could Not find one of the players. Please check spelling, and try again'
+					);
+				} else {
+					setP1Data(response.data.p1Data);
+					setP2Data(response.data.p2Data);
+					setP1Stats(response.data.p1Stats);
+					setP2Stats(response.data.p2Stats);
+				}
+			})
+			.catch((error) => {
+				console.log(error);
+				toast.error('Players could not be found!');
+			});
+	};
 
 	useEffect(() => {
 		if (typeof window !== 'undefined') {
@@ -75,114 +79,125 @@ const PlayerDetails = () => {
 				})
 				.catch((error) => {
 					console.error('Error fetching data:', error);
-                    toast.error('Player Not Found!');
+					toast.error('Player Not Found!');
 				});
 		}
 	}, [first, last]);
 
-    const shotPercentage = (made, attempted)=>{
-        let percentage = made / attempted * 100
-        return `${percentage.toFixed(2)}%`
-    }
+	const shotPercentage = (made, attempted) => {
+		let percentage = (made / attempted) * 100;
+		return `${percentage.toFixed(2)}%`;
+	};
 
-console.log(p1Data, p1Stats)
-console.log(p2Data, p2Stats)
+	console.log(p1Data, p1Stats);
+	console.log(p2Data, p2Stats);
 	return (
 		<div className='playerPage'>
-            <ToastContainer/>
+			<ToastContainer />
 			{playerDetails.length === 0 && p1Data.length === 0 ? (
 				<div>
-				<div className='searchPlayer'>
-					<h3>Search Player Stats:</h3>
-                    {/* <p>Note: Some Rookies have not been added yet :(</p> */}
-					<p>
-						<input
-							type='text'
-							name='firstName'
-							placeholder='First Name'
-							onChange={(e) => setFirstName(e.target.value)}
-						/>
-					</p>
-					<p>
-						<input
-							type='text'
-							name='lastName'
-							placeholder='Last Name'
-							onChange={(e) => setLastName(e.target.value)}
-						/>
-					</p>
-					<p>
-						<button onClick={() => handleSubmit()}>Search</button>
-					</p>
-				</div>
-				<div className='comparePlayers'>
-					<h3>Compare Player Stats</h3>
-					<div>
-						<h5>Player One:</h5>
-					<p>
-						<input
-							type='text'
-							name='firstName'
-							placeholder='First Name'
-							onChange={(e) => setP1FirstName(e.target.value)}
-						/>
-					</p>
-					<p>
-						<input
-							type='text'
-							name='lastName'
-							placeholder='Last Name'
-							onChange={(e) => setP1LastName(e.target.value)}
-						/>
-					</p>
+					<div className='searchPlayer'>
+						<h3>Search Player Stats:</h3>
+						{/* <p>Note: Some Rookies have not been added yet :(</p> */}
+						<p>
+							<input
+								type='text'
+								name='firstName'
+								placeholder='First Name'
+								onChange={(e) => setFirstName(e.target.value)}
+							/>
+						</p>
+						<p>
+							<input
+								type='text'
+								name='lastName'
+								placeholder='Last Name'
+								onChange={(e) => setLastName(e.target.value)}
+							/>
+						</p>
+						<p>
+							<button onClick={() => handleSubmit()}>Search</button>
+						</p>
 					</div>
-					<div>
-						<h5>Player Two:</h5>
-					<p>
-						<input
-							type='text'
-							name='firstName'
-							placeholder='First Name'
-							onChange={(e) => setP2FirstName(e.target.value)}
-						/>
-					</p>
-					<p>
-						<input
-							type='text'
-							name='lastName'
-							placeholder='Last Name'
-							onChange={(e) => setP2LastName(e.target.value)}
-						/>
-					</p>
+					<div className='comparePlayers'>
+						<h3>Compare Player Stats</h3>
+						<div>
+							<h5>Player One:</h5>
+							<p>
+								<input
+									type='text'
+									name='firstName'
+									placeholder='First Name'
+									onChange={(e) => setP1FirstName(e.target.value)}
+								/>
+							</p>
+							<p>
+								<input
+									type='text'
+									name='lastName'
+									placeholder='Last Name'
+									onChange={(e) => setP1LastName(e.target.value)}
+								/>
+							</p>
+						</div>
+						<div>
+							<h5>Player Two:</h5>
+							<p>
+								<input
+									type='text'
+									name='firstName'
+									placeholder='First Name'
+									onChange={(e) => setP2FirstName(e.target.value)}
+								/>
+							</p>
+							<p>
+								<input
+									type='text'
+									name='lastName'
+									placeholder='Last Name'
+									onChange={(e) => setP2LastName(e.target.value)}
+								/>
+							</p>
+						</div>
+						<button onClick={() => comparePlayers()}>Compare Players</button>
 					</div>
-					<button onClick={()=>comparePlayers()}>Compare Players</button>
 				</div>
-				</div>
-			) : p1Data.length === 0 ?(
+			) : p1Data.length === 0 ? (
 				<div className='playerDetails'>
 					<div className='playerBio'>
-                        {playerDetails.details ?(
-                            <>
-						<Image
-							src={playerDetails.details.player[0].strThumb}
-							className='playerPic'
-							alt='player Picture'
-						/>
-						<div>
-							<h3>{playerDetails.details.player[0].strPlayer}</h3>
-							<h3>{playerDetails.details.player[0].strPosition}</h3>
-							<h3>{playerDetails.details.player[0].strHeight}</h3>
-							<h3><a href={`/TeamDetails?teamName=${playerDetails.playerData[0].team.full_name}`}>{playerDetails.details.player[0].strTeam}</a></h3>
-						</div>
-                            </>
-
-                        ):(
-                            <>
-                            <h3>{playerDetails.playerData[0].first_name} {playerDetails.playerData[0].last_name}</h3>
-                            <h3><a href={`/TeamDetails?teamName=${playerDetails.playerData[0].team.full_name}`}>{playerDetails.playerData[0].team.full_name}</a></h3>
-                            </>
-                            
-                        )}
+						{playerDetails.details ? (
+							<>
+								<Image
+									src={playerDetails.details.player[0].strThumb}
+									className='playerPic'
+									alt='player Picture'
+								/>
+								<div>
+									<h3>{playerDetails.details.player[0].strPlayer}</h3>
+									<h3>{playerDetails.details.player[0].strPosition}</h3>
+									<h3>{playerDetails.details.player[0].strHeight}</h3>
+									<h3>
+										<a
+											href={`/TeamDetails?teamName=${playerDetails.playerData[0].team.full_name}`}>
+											{playerDetails.details.player[0].strTeam}
+										</a>
+									</h3>
+								</div>
+							</>
+						) : (
+							<>
+								<h3>
+									{playerDetails.playerData[0].first_name}{' '}
+									{playerDetails.playerData[0].last_name}
+								</h3>
+								<h3>
+									<a
+										href={`/TeamDetails?teamName=${playerDetails.playerData[0].team.full_name}`}>
+										{playerDetails.playerData[0].team.full_name}
+									</a>
+								</h3>
+							</>
+						)}
 					</div>
 					<div className='playerStats'>
 						<Table striped='columns'>
@@ -223,9 +238,30 @@ console.log(p2Data, p2Stats)
 											{playerDetails.stats[0].games_played}
 										</td>
 										<td>{playerDetails.stats[0].min}</td>
-										<td>{playerDetails.stats[0].fgm === 0 ? `0%`: shotPercentage(playerDetails.stats[0].fgm, playerDetails.stats[0].fga)}</td>
-										<td>{playerDetails.stats[0].fg3m === 0 ? `0%`: shotPercentage(playerDetails.stats[0].fg3m,playerDetails.stats[0].fg3a)}</td>
-										<td>{playerDetails.stats[0].ftm === 0 ? `0%`: shotPercentage(playerDetails.stats[0].ftm,playerDetails.stats[0].fta)}</td>
+										<td>
+											{playerDetails.stats[0].fgm === 0
+												? `0%`
+												: shotPercentage(
+														playerDetails.stats[0].fgm,
+														playerDetails.stats[0].fga
+												  )}
+										</td>
+										<td>
+											{playerDetails.stats[0].fg3m === 0
+												? `0%`
+												: shotPercentage(
+														playerDetails.stats[0].fg3m,
+														playerDetails.stats[0].fg3a
+												  )}
+										</td>
+										<td>
+											{playerDetails.stats[0].ftm === 0
+												? `0%`
+												: shotPercentage(
+														playerDetails.stats[0].ftm,
+														playerDetails.stats[0].fta
+												  )}
+										</td>
 										<td>{playerDetails.stats[0].pts}</td>
 										<td>{playerDetails.stats[0].reb}</td>
 										<td>{playerDetails.stats[0].ast}</td>
@@ -245,12 +281,11 @@ console.log(p2Data, p2Stats)
 					</div>
 					{!showGamelog ? (
 						<div className='description'>
-                            {playerDetails.details ?(
-                                <p>{playerDetails.details.player[0].strDescriptionEN}</p>
-
-                            ):(
-                                <h3>No Player Details Available</h3>
-                            )}
+							{playerDetails.details ? (
+								<p>{playerDetails.details.player[0].strDescriptionEN}</p>
+							) : (
+								<h3>No Player Details Available</h3>
+							)}
 						</div>
 					) : playerDetails.gamelog.length > 0 ? (
 						<div className='gamelog'>
@@ -295,36 +330,58 @@ console.log(p2Data, p2Stats)
 						<h2>No Games Played Yet</h2>
 					)}
 				</div>
-			):(
+			) : (
 				<div>
 					<Table className='comparePlayersTable'>
 						<thead>
 							<tr>
-								<th>{p1Data[0].first_name} {p1Data[0].last_name} vs {p2Data[0].first_name} {p2Data[0].last_name}</th>
 								<th>
-										<p><a href={`/PlayerDetails/?first=${p1Data[0].first_name}&last=${p1Data[0].last_name}`}>{p1Data[0].first_name} {p1Data[0].last_name}</a></p>
-										<p>{p1Data[0].position}</p>
-										{p1Data[0].height_feet && p1Data[0].height_inches ?(
-											<p>{p1Data[0].height_feet}ft {p1Data[0].height_inches}in</p>
-
-										):(
-											<p>Height Not Found</p>
-										)
-										}
-										<p><a href={`/TeamDetails?teamName=${p1Data[0].team.full_name}`}>{p1Data[0].team.full_name}</a></p>
-									
+									{p1Data[0].first_name} {p1Data[0].last_name} vs{' '}
+									{p2Data[0].first_name} {p2Data[0].last_name}
 								</th>
 								<th>
-										<p><a href={`/PlayerDetails/?first=${p2Data[0].first_name}&last=${p2Data[0].last_name}`}>{p2Data[0].first_name} {p2Data[0].last_name}</a></p>
-										<p>{p2Data[0].position}</p>
-										{p2Data[0].height_feet && p2Data[0].height_inches ?(
-											<p>{p2Data[0].height_feet}ft {p2Data[0].height_inches}in</p>
-
-										):(
-											<p>Height Not Found</p>
-										)
-										}
-										<p><a href={`/TeamDetails?teamName=${p2Data[0].team.full_name}`}>{p2Data[0].team.full_name}</a></p>
+									<p>
+										<a
+											href={`/PlayerDetails/?first=${p1Data[0].first_name}&last=${p1Data[0].last_name}`}>
+											{p1Data[0].first_name} {p1Data[0].last_name}
+										</a>
+									</p>
+									<p>{p1Data[0].position}</p>
+									{p1Data[0].height_feet && p1Data[0].height_inches ? (
+										<p>
+											{p1Data[0].height_feet}ft {p1Data[0].height_inches}in
+										</p>
+									) : (
+										<p>Height Not Found</p>
+									)}
+									<p>
+										<a
+											href={`/TeamDetails?teamName=${p1Data[0].team.full_name}`}>
+											{p1Data[0].team.full_name}
+										</a>
+									</p>
+								</th>
+								<th>
+									<p>
+										<a
+											href={`/PlayerDetails/?first=${p2Data[0].first_name}&last=${p2Data[0].last_name}`}>
+											{p2Data[0].first_name} {p2Data[0].last_name}
+										</a>
+									</p>
+									<p>{p2Data[0].position}</p>
+									{p2Data[0].height_feet && p2Data[0].height_inches ? (
+										<p>
+											{p2Data[0].height_feet}ft {p2Data[0].height_inches}in
+										</p>
+									) : (
+										<p>Height Not Found</p>
+									)}
+									<p>
+										<a
+											href={`/TeamDetails?teamName=${p2Data[0].team.full_name}`}>
+											{p2Data[0].team.full_name}
+										</a>
+									</p>
 								</th>
 							</tr>
 						</thead>
@@ -333,55 +390,165 @@ console.log(p2Data, p2Stats)
 								<td>
 									<strong>Games Played</strong>
 								</td>
-								<td style={{backgroundColor:p1Stats[0].games_played > p2Stats[0].games_played ? 'green' : "red"}}>{p1Stats[0].games_played}</td>
-								<td style={{backgroundColor:p2Stats[0].games_played > p1Stats[0].games_played ? 'green' : "red"}}>{p2Stats[0].games_played}</td>
+								<td
+									style={{
+										backgroundColor:
+											p1Stats[0].games_played > p2Stats[0].games_played
+												? 'green'
+												: 'red',
+									}}>
+									{p1Stats[0].games_played}
+								</td>
+								<td
+									style={{
+										backgroundColor:
+											p2Stats[0].games_played > p1Stats[0].games_played
+												? 'green'
+												: 'red',
+									}}>
+									{p2Stats[0].games_played}
+								</td>
 							</tr>
 							<tr>
 								<td>
-									<strong>Minutes Per Game</strong> 
+									<strong>Minutes Per Game</strong>
 								</td>
-								<td style={{backgroundColor:p1Stats[0].min > p2Stats[0].min ? 'green' : "red"}}>{p1Stats[0].min}</td>
-								<td style={{backgroundColor:p2Stats[0].min > p1Stats[0].min ? 'green' : "red"}}>{p2Stats[0].min}</td>
+								<td
+									style={{
+										backgroundColor:
+											p1Stats[0].min > p2Stats[0].min ? 'green' : 'red',
+									}}>
+									{p1Stats[0].min}
+								</td>
+								<td
+									style={{
+										backgroundColor:
+											p2Stats[0].min > p1Stats[0].min ? 'green' : 'red',
+									}}>
+									{p2Stats[0].min}
+								</td>
 							</tr>
 							<tr>
 								<td>
-									<strong>Points Per Game</strong> 
+									<strong>Points Per Game</strong>
 								</td>
-								<td style={{backgroundColor:p1Stats[0].pts > p2Stats[0].pts ? 'green' : "red"}}>{p1Stats[0].pts}</td>
-								<td style={{backgroundColor:p2Stats[0].pts > p1Stats[0].pts ? 'green' : "red"}}>{p2Stats[0].pts}</td>
+								<td
+									style={{
+										backgroundColor:
+											p1Stats[0].pts > p2Stats[0].pts ? 'green' : 'red',
+									}}>
+									{p1Stats[0].pts}
+								</td>
+								<td
+									style={{
+										backgroundColor:
+											p2Stats[0].pts > p1Stats[0].pts ? 'green' : 'red',
+									}}>
+									{p2Stats[0].pts}
+								</td>
 							</tr>
 							<tr>
 								<td>
-									<strong>Rebounds Per Game</strong> 
+									<strong>Rebounds Per Game</strong>
 								</td>
-								<td style={{backgroundColor:p1Stats[0].reb > p2Stats[0].reb ? 'green' : "red"}}>{p1Stats[0].reb}</td>
-								<td style={{backgroundColor:p2Stats[0].reb > p1Stats[0].reb ? 'green' : "red"}}>{p2Stats[0].reb}</td>
+								<td
+									style={{
+										backgroundColor:
+											p1Stats[0].reb > p2Stats[0].reb ? 'green' : 'red',
+									}}>
+									{p1Stats[0].reb}
+								</td>
+								<td
+									style={{
+										backgroundColor:
+											p2Stats[0].reb > p1Stats[0].reb ? 'green' : 'red',
+									}}>
+									{p2Stats[0].reb}
+								</td>
 							</tr>
 							<tr>
 								<td>
-									<strong>Assists Per Game</strong> 
+									<strong>Assists Per Game</strong>
 								</td>
-								<td style={{backgroundColor:p1Stats[0].ast > p2Stats[0].ast ? 'green' : "red"}}>{p1Stats[0].ast}</td>
-								<td style={{backgroundColor:p2Stats[0].ast > p1Stats[0].ast ? 'green' : "red"}}>{p2Stats[0].ast}</td>
+								<td
+									style={{
+										backgroundColor:
+											p1Stats[0].ast > p2Stats[0].ast ? 'green' : 'red',
+									}}>
+									{p1Stats[0].ast}
+								</td>
+								<td
+									style={{
+										backgroundColor:
+											p2Stats[0].ast > p1Stats[0].ast ? 'green' : 'red',
+									}}>
+									{p2Stats[0].ast}
+								</td>
 							</tr>
 							<tr>
 								<td>
-									<strong>Steals Per Game</strong> 
+									<strong>Steals Per Game</strong>
 								</td>
-								<td style={{backgroundColor:p1Stats[0].stl > p2Stats[0].stl ? 'green' : "red"}}>{p1Stats[0].stl}</td>
-								<td style={{backgroundColor:p2Stats[0].stl > p1Stats[0].stl ? 'green' : "red"}}>{p2Stats[0].stl}</td>
+								<td
+									style={{
+										backgroundColor:
+											p1Stats[0].stl > p2Stats[0].stl ? 'green' : 'red',
+									}}>
+									{p1Stats[0].stl}
+								</td>
+								<td
+									style={{
+										backgroundColor:
+											p2Stats[0].stl > p1Stats[0].stl ? 'green' : 'red',
+									}}>
+									{p2Stats[0].stl}
+								</td>
 							</tr>
 							<tr>
 								<td>
-									<strong>Turnovers Per Game</strong> 
+									<strong>Blocks Per Game</strong>
 								</td>
-								<td style={{backgroundColor:p1Stats[0].turnover < p2Stats[0].turnover ? 'green' : "red"}}>{p1Stats[0].turnover}</td>
-								<td style={{backgroundColor:p2Stats[0].turnover < p1Stats[0].turnover ? 'green' : "red"}}>{p2Stats[0].turnover}</td>
+								<td
+									style={{
+										backgroundColor:
+											p1Stats[0].blk > p2Stats[0].blk ? 'green' : 'red',
+									}}>
+									{p1Stats[0].blk}
+								</td>
+								<td
+									style={{
+										backgroundColor:
+											p2Stats[0].blk > p1Stats[0].blk ? 'green' : 'red',
+									}}>
+									{p2Stats[0].blk}
+								</td>
+							</tr>
+							<tr>
+								<td>
+									<strong>Turnovers Per Game</strong>
+								</td>
+								<td
+									style={{
+										backgroundColor:
+											p1Stats[0].turnover < p2Stats[0].turnover
+												? 'green'
+												: 'red',
+									}}>
+									{p1Stats[0].turnover}
+								</td>
+								<td
+									style={{
+										backgroundColor:
+											p2Stats[0].turnover < p1Stats[0].turnover
+												? 'green'
+												: 'red',
+									}}>
+									{p2Stats[0].turnover}
+								</td>
 							</tr>
 						</tbody>
 					</Table>
 				</div>
-				
 			)}
 		</div>
 	);
