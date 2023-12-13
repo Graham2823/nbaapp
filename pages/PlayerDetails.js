d775b0fedac1a2851aa6ea53d22ff594e5ad0fad
Graham <gmfreundlich@gmail.com>
2023-12-13 09:21:23 -0500

    fixed nba standings api key

--- pages/PlayerDetails.js
+++ pages/PlayerDetails.js
@@ -26,7 +26,7 @@ const PlayerDetails = () => {
 		if(first && last){
 			axios
 			.get(
-				`https://nbaapp.vercel.app/api/getPlayer?firstName=${first}&lastName=${last}`
+				`http://localhost:3000/api/getPlayer?firstName=${first}&lastName=${last}`
 			)
 			.then((response) => {
 				setPlayerDetails(response.data);
--- pages/PlayerSearch.js
+++ pages/PlayerSearch.js
@@ -4,44 +4,81 @@ import { useRouter } from 'next/router';
 import '../app/app.css';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer, toast } from 'react-toastify';
+import { Dropdown, Overlay, Tooltip } from 'react-bootstrap';
 
 const PlayerSearch = () => {
-    const router = useRouter()
-    const {error} = router.query
+	const router = useRouter();
+	const { error } = router.query;
 	const [firstName, setFirstName] = useState('');
 	const [lastName, setLastName] = useState('');
-	const [p1FirstName, setP1FirstName] = useState('')
-	const [p1LastName, setP1LastName] = useState('')
-	const [p2FirstName, setP2FirstName] = useState('')
-	const [p2LastName, setP2LastName] = useState('')
+	const [p1FirstName, setP1FirstName] = useState('');
+	const [p1LastName, setP1LastName] = useState('');
+	const [p2FirstName, setP2FirstName] = useState('');
+	const [p2LastName, setP2LastName] = useState('');
+	const [name, setName] = useState();
+	const [filteredPlayers, setFilteredPlayers] = useState([]);
+	const [showDropdown, setShowDropdown] = useState(false);
+	const [target, setTarget] = useState(null);
+	const [selectedPlayer, setSelectedPlayer] = useState()
 
-    useEffect(()=>{
-        if(error){
-            if(error === "PlayerNotFound"){
-                toast.error('Player Not Found, try again!')
-            }else{
-                toast.error('One of the players was not found, try again!')
-            }
-        }
-    },[error])
+	useEffect(() => {
+		if (error) {
+			if (error === 'PlayerNotFound') {
+				toast.error('Player Not Found, try again!');
+			} else {
+				toast.error('One of the players was not found, try again!');
+			}
+		}
+	}, [error]);
 
-
-	const handleSubmit = () => {
-		router.push(`/PlayerDetails/?first=${firstName}&last=${lastName}`)
+	const handleInputChange = (e) => {
+		setName(e.target.value);
+		setTarget(e.target);
 	};
 
-	const comparePlayers = () =>{
-		router.push(`/PlayerDetails?p1Name=${p1FirstName}_${p1LastName}&p2Name=${p2FirstName}_${p2LastName}`)
-	}
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await axios.get(
+					`http://localhost:3000/api/findPlayers?name=${name}`
+				);
+				setFilteredPlayers(response.data.playersToDisplay);
+				setShowDropdown(true);
+			} catch (error) {
+				console.error('Error fetching players:', error);
+			}
+		};
+
+		fetchData();
+	}, [name]);
+
+	const handleSubmit = () => {
+		router.push(`/PlayerDetails/?first=${name.split(" ")[0]}&last=${name.split(" ")[1]}`);
+	};
+
+	const comparePlayers = () => {
+		router.push(
+			`/PlayerDetails?p1Name=${p1FirstName}_${p1LastName}&p2Name=${p2FirstName}_${p2LastName}`
+		);
+	};
+	const handleSelectPlayer = (e) => {
+		const playerName = e.target.textContent;
+		setSelectedPlayer(playerName);
+		setName(playerName);
+		setShowDropdown(false);
+	  };
+
+
+	console.log(selectedPlayer);
 
 	return (
 		<div className='playerPage'>
-            <ToastContainer/>
-				<div>
+			<ToastContainer />
+			<div>
 				<div className='searchPlayer'>
 					<h3>Search Player Stats:</h3>
-                    {/* <p>Note: Some Rookies have not been added yet :(</p> */}
-					<p>
+					{/* <p>Note: Some Rookies have not been added yet :(</p> */}
+					{/* <p>
 						<input
 							type='text'
 							name='firstName'
@@ -56,7 +93,31 @@ const PlayerSearch = () => {
 							placeholder='Last Name'
 							onChange={(e) => setLastName(e.target.value)}
 						/>
+					</p> */}
+					<p>
+						<input
+							type='text'
+							name='name'
+							onChange={(e) => handleInputChange(e)} autoComplete='off' value={name}></input>
+						<Overlay
+							show={showDropdown}
+							target={target}
+							placement='bottom-start'
+						>
+							{(props) => (
+								<Tooltip id='overlay-example' {...props}>
+									<Dropdown.Menu show={showDropdown}>
+										{filteredPlayers.map((player, index) => (
+											<Dropdown.Item key={index} value={player.name} onClick={(e)=> handleSelectPlayer(e)}>
+												{player.name}
+											</Dropdown.Item>
+										))}
+									</Dropdown.Menu>
+								</Tooltip>
+							)}
+						</Overlay>
 					</p>
+
 					<p>
 						<button onClick={() => handleSubmit()}>Search</button>
 					</p>
@@ -65,47 +126,47 @@ const PlayerSearch = () => {
 					<h3>Compare Player Stats</h3>
 					<div>
 						<h5>Player One:</h5>
-					<p>
-						<input
-							type='text'
-							name='firstName'
-							placeholder='First Name'
-							onChange={(e) => setP1FirstName(e.target.value)}
-						/>
-					</p>
-					<p>
-						<input
-							type='text'
-							name='lastName'
-							placeholder='Last Name'
-							onChange={(e) => setP1LastName(e.target.value)}
-						/>
-					</p>
+						<p>
+							<input
+								type='text'
+								name='firstName'
+								placeholder='First Name'
+								onChange={(e) => setP1FirstName(e.target.value)}
+							/>
+						</p>
+						<p>
+							<input
+								type='text'
+								name='lastName'
+								placeholder='Last Name'
+								onChange={(e) => setP1LastName(e.target.value)}
+							/>
+						</p>
 					</div>
 					<div>
 						<h5>Player Two:</h5>
-					<p>
-						<input
-							type='text'
-							name='firstName'
-							placeholder='First Name'
-							onChange={(e) => setP2FirstName(e.target.value)}
-						/>
-					</p>
-					<p>
-						<input
-							type='text'
-							name='lastName'
-							placeholder='Last Name'
-							onChange={(e) => setP2LastName(e.target.value)}
-						/>
-					</p>
+						<p>
+							<input
+								type='text'
+								name='firstName'
+								placeholder='First Name'
+								onChange={(e) => setP2FirstName(e.target.value)}
+							/>
+						</p>
+						<p>
+							<input
+								type='text'
+								name='lastName'
+								placeholder='Last Name'
+								onChange={(e) => setP2LastName(e.target.value)}
+							/>
+						</p>
 					</div>
-					<button onClick={()=>comparePlayers()}>Compare Players</button>
+					<button onClick={() => comparePlayers()}>Compare Players</button>
 				</div>
-				</div>
+			</div>
 		</div>
 	);
-}
+};
 
-export default PlayerSearch
+export default PlayerSearch;
