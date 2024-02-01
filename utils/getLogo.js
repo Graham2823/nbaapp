20adca7136620f68075a816ac90ce59abe2c3c35
Graham <gmfreundlich@gmail.com>
2024-02-01 10:27:33 -0500

    added betting odds to todays games

--- utils/getLogo.js
+++ utils/getLogo.js
@@ -1,8 +1,6 @@
 import teams from "@/teams"
 
 export default function getTeamLogo(teamName){
-  console.log(teamName)
     const team = teams.filter((team)=> team.teamName === teamName)
-    console.log("team", team)
     return `/${team[0].teamLogo}`
   }
