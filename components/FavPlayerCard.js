1f17b27d26984f4a82e284395f8a0465d71d7286
Graham <gmfreundlich@gmail.com>
2024-03-03 14:22:42 -0500

    fixed favorite players and teams

--- components/FavPlayerCard.js
+++ components/FavPlayerCard.js
@@ -16,8 +16,9 @@ const FavPlayerCard = ({favoritePlayer}) => {
         axios
             .get(`https://nbaapp.vercel.app/api/getPlayer?firstName=${favoritePlayer.playerName.split(" ")[0]}&lastName=${favoritePlayer.playerName.split(" ")[1]}${favoritePlayer.playerName.split(" ")[2]? "%20"+ favoritePlayer.playerName.split(" ")[2] : ''}`)
             .then((response) => {
+                console.log(response)
                 setPlayerDetails(response.data);
-                setLast5Game(response.data.gamelog.slice(-5).reverse())
+                setLast5Game(response.data.playerGamelog.slice(-5).reverse())
             })
             .catch((error) => {
                 console.error('Error fetching data:', error);
@@ -59,19 +60,19 @@ const FavPlayerCard = ({favoritePlayer}) => {
         {showPlayerStats ? (
             <tbody>
         <tr>
-            <td>Points:</td><td>{playerDetails.stats[0][0].pts}</td>
+            <td>Points:</td><td>{playerDetails.playerAverages[0].pts}</td>
             </tr>
             <tr>
-            <td>Rebounds:</td><td>{playerDetails.stats[0][0].reb}</td>
+            <td>Rebounds:</td><td>{playerDetails.playerAverages[0].reb}</td>
             </tr>
             <tr>
-            <td>Assists:</td><td>{playerDetails.stats[0][0].ast}</td>
+            <td>Assists:</td><td>{playerDetails.playerAverages[0].ast}</td>
             </tr>
             <tr>
-            <td>Steals:</td><td>{playerDetails.stats[0][0].stl}</td>
+            <td>Steals:</td><td>{playerDetails.playerAverages[0].stl}</td>
             </tr>
             <tr>
-            <td>Blocks:</td><td>{playerDetails.stats[0][0].blk}</td>
+            <td>Blocks:</td><td>{playerDetails.playerAverages[0].blk}</td>
             </tr>
                 </tbody>
             ): showPlayerDetails ? (
